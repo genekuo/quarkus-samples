530a1bacab2f3deca7f875212441500831dcb0b3
Gene Kuo <genekuo@GenedeMacBook-Pro.local>
2021-05-21 17:37:53 +0800

    Add reactive and postgres-reactive

---
+++ postgres-reactive/src/main/java/demo/quarkus/postgres/reactive/FruitResource.java
@@ -0,0 +1,58 @@
+package demo.quarkus.postgres.reactive;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+@Path("fruits")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FruitResource {
+
+    private final PgPool client;
+
+    public FruitResource(PgPool client) {
+        this.client = client;
+    }
+
+    @GET
+    public Multi<Fruit> get() {
+        return Fruit.findAll(client);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingle(Long id) {
+        return Fruit.findById(client, id)
+                .onItem().transform(fruit -> fruit != null ? Response.ok(fruit) : Response.status(Response.Status.NOT_FOUND))
+                .onItem().transform(Response.ResponseBuilder::build);
+    }
+
+    @POST
+    public Uni<Response> create(Fruit fruit) {
+        return fruit.save(client)
+                .onItem().transform(id -> URI.create("/fruits/" + id))
+                .onItem().transform(uri -> Response.created(uri).build());
+    }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> update(Fruit fruit) {
+        return fruit.update(client)
+                .onItem().transform(updated -> updated ? Response.Status.OK : Response.Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(Long id) {
+        return Fruit.delete(client, id)
+                .onItem().transform(deleted -> deleted ? Response.Status.NO_CONTENT : Response.Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
+}
---
+++ postgres-reactive/src/test/java/demo/quarkus/postgres/reactive/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package demo.quarkus.postgres.reactive;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
