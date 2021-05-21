adc81abc73dab8e8778f0fc4c364ae1e23a75888
Gene Kuo <genekuo@GenedeMacBook-Pro.local>
2021-05-21 23:59:09 +0800

    Add stock-trading

---
+++ stock-trading/src/main/java/demo/quarkus/stock/kafka/Company.java
@@ -0,0 +1,10 @@
+package demo.quarkus.stock.kafka;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public enum Company {
+        Acme, Globex, Umbrella, Soylent, Initech
+}
+
+
---
+++ stock-trading/src/main/java/demo/quarkus/stock/kafka/Quote.java
@@ -0,0 +1,35 @@
+package demo.quarkus.stock.kafka;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+
+@RegisterForReflection
+public class Quote   {
+    String company;
+    Double value;
+
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Quote(String company, Double value) {
+        this.company = company;
+        this.value = value;
+    }
+
+    public Quote() {
+    }
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+}
